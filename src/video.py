4079bd0e29fb32789fabc4a3f0ad44b42c3533a9
vanila <idegtiareov@mail.ru>
2023-08-16 02:42:05 +0300

    HomeWork 6

--- src/video.py
+++ src/video.py
@@ -1,16 +1,33 @@
 from src.channel import Channel
 
+class WrongVideoID(Exception):
+    def __init__(self):
+        self.message = 'Неправильный видео id:'
+
+
 class Video:
     def __init__(self, video_id):
-        self.video_id = video_id
-        video_response = Channel.get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails',id=video_id).execute()
-        self.video_title: str = video_response['items'][0]['snippet']['title']
-        self.url: str = f'https://www.youtube.com/watch?v={video_id}'
-        self.view_count: int = video_response['items'][0]['statistics']['viewCount']
-        self.like_count: int = video_response['items'][0]['statistics']['likeCount']
+        try:
+            self.video_id = video_id
+            self.video_response = Channel.get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails',id=video_id).execute()
+            if self.video_response['pageInfo']['totalResults'] > 0:
+                self.title: str = self.video_response['items'][0]['snippet']['title']
+                self.url: str = f'https://www.youtube.com/watch?v={video_id}'
+                self.view_count: int = self.video_response['items'][0]['statistics']['viewCount']
+                self.like_count: int = self.video_response['items'][0]['statistics']['likeCount']
+            else:
+                self.video_response = None
+                self.title = None
+                self.url = None
+                self.view_count = None
+                self.like_count = None
+                raise WrongVideoID
+
+        except WrongVideoID as ex:
+            print(f'{ex.message} {video_id}')
 
     def __str__(self):
-        return self.video_title
+        return self.title
 
 
 class PLVideo(Video):
